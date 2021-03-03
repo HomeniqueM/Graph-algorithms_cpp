47997aa6e83c202d387e762d2bfa8feef0780abd
Homenique Vieira <homenique.source@gmail.com>
2021-03-03 15:03:51 -0300

    Created Weighted directed graph(WDG) class

--- src/models/WDG.cpp
+++ src/models/WDG.cpp
@@ -1,5 +1,6 @@
 #include "headers/WDG.hpp"
 
+//Weighted directed graph
 void WDG::newRelation(int value, int adjValue, int weight) {
   
   newVertice(value);
--- src/models/headers/WDG.hpp
+++ src/models/headers/WDG.hpp
@@ -2,6 +2,7 @@
 #define WDG_hpp
 #include "../../graph/headers/AdjacencyList.hpp"
 
+//Weighted directed graph
 class WDG : public AdjacencyList {
   public:
   void newRelation(int value, int adjValue, int weight);
